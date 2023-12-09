90d7cedf2165bff6e890ce9fc11d7dea43dbd0b4
thomasvdd9123 <55441231+thomasvdd9123@users.noreply.github.com>
2023-12-09 20:54:17 +0100

    feat: changed message

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,14 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "3DPrintPlatform": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "CreatedSNSRole": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
