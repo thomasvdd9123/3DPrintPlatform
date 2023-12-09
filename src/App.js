0fc31abb3c99b32e6ee834c208220cea919c63bd
thomasvdd9123 <55441231+thomasvdd9123@users.noreply.github.com>
2023-12-09 22:12:02 +0100

    feat: changed message

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { Amplify } from 'aws-amplify';
+
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import config from './amplifyconfiguration.json';
 Amplify.configure(config);
 
-export function App({ signOut, user }) {
+function App({ signOut, user }) {
   return (
     <>
-      <h1>Hello {user?.username}</h1>
+      <h1>Hello {user.username}</h1>
       <button onClick={signOut}>Sign out</button>
     </>
   );
